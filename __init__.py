6667ee68b42af8144b65e9ffb755b803d6e55dae
timotiaztec <timotiaztec@gmail.com>
2021-02-05 16:53:13 +0200

    Update

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ import curses
 screen = curses.initscr()
 curses.start_color()
 
-if sys.argv[1]:
+if len(sys.argv) == 2:
     if sys.argv[1].upper() == "GB":
         divisionType = "Gb"
         division = 1000000000
@@ -25,6 +25,22 @@ else:
     divisionType = "Bytes"
     division = 0
 
+
+def get_size(start_path='.'):
+    total_size = 0
+    if os.path.isfile(start_path):
+        total_size = os.path.getsize(start_path)
+    else:
+        for dirpath, dirnames, filenames in os.walk(start_path):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                # skip if it is symbolic link
+                if not os.path.islink(fp):
+                    total_size += os.path.getsize(fp)
+
+    return total_size
+
+
 screen.clear()
 with os.scandir() as dir_entries:
     files = []
@@ -32,9 +48,9 @@ with os.scandir() as dir_entries:
     sumSize = 0
     for dir_entry, name in zip(dir_entries, os.listdir()):
         if division > 0:
-            size = dir_entry.stat().st_size / division
+            size = get_size(name) / division
         else:
-            size = dir_entry.stat().st_size
+            size = get_size(name)
         sumSize += size
         sizes.append(size)
         files.append({'size': size, 'name': name})
@@ -43,15 +59,17 @@ with os.scandir() as dir_entries:
     avgSize = sumSize / len(files)
     screen.addstr("Files in directory: \n", curses.A_BOLD)
     for item in files:
-        screen.addstr("    {}".format(item.get('name', 'Some file')) + (" " * (20 - len(item['name']))))
-        if item.get('size') < avgSize:
-            screen.addstr("| {} {}".format(item.get('size', 'can`t get size'), divisionType)+"\n", curses.A_DIM)
-        elif item.get('size') > avgSize:
-            screen.addstr("| {} {}".format(item.get('size', 'can`t get size'), divisionType)+"\n", curses.A_BOLD)
-        elif item.get('size') > (maxSize - avgSize):
-            screen.addstr("| {} {}".format(item.get('size', 'can`t get size'), divisionType) + "\n", curses.A_STANDOUT)
+        screen.addstr("    {}".format(item.get('name', 'Some file')) + (" " * (20 - len(item['name']))) + "| ")
+        if item.get('size') < (avgSize - maxSize):
+            screen.addstr("{} {}".format(item.get('size', 'can`t get size'), divisionType) + "\n", curses.A_DIM)
+        elif item.get('size') > (maxSize - (avgSize / 3)) or item.get('size') == maxSize:
+            screen.addstr("{} {}".format(item.get('size', 'can`t get size'), divisionType) + "\n", curses.A_STANDOUT)
+        elif item.get('size') > (avgSize + (avgSize / 3)):
+            screen.addstr("{} {}".format(item.get('size', 'can`t get size'), divisionType) + "\n", curses.A_BLINK)
+        elif item.get('size') > avgSize or item.get('size') > (avgSize - (avgSize / 3)):
+            screen.addstr("{} {}".format(item.get('size', 'can`t get size'), divisionType) + "\n", curses.A_BOLD)
         else:
-            screen.addstr("| {} {}".format(item.get('size', 'can`t get size'), divisionType) + "\n")
+            screen.addstr("{} {}".format(item.get('size', 'can`t get size'), divisionType) + "\n")
     screen.addstr("Total: {} {}".format(sumSize, divisionType), curses.A_BOLD)
 
 c = screen.getch()
